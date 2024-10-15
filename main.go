800df775a32b3fd768ace9ebe66c0996fc886668
ChanchalS7 <chsverma7@gmail.com>
2024-10-15 14:44:26 +0530

    added project structure

--- main.go
+++ main.go
@@ -5,6 +5,33 @@ import (
 	"fmt"
 )
 //1.Create web server
+//2. HTTP Verbs
+/*
+-GET
+-POST
+-PUT
+-PATCH
+-DELETE
+*/
+//3.REST ARCHITECTURE
+/*
+-Client-server architecture
+- stateless communication
+- uniform interface
+- 
+*/
+//4. Project explanation
+/*
+- product api
+-[id,name,quantity,price]
+-database
+- GET ->/products
+- GET ->/products/id
+- POST ->/product
+- PUT ->/product/id
+- DELETE ->/product/id
+
+*/
 
 func homepage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Welcome to homepage")

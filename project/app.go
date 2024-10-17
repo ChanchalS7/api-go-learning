e406e46731d50bd68dfee220f09d36459bf12d74
ChanchalS7 <chsverma7@gmail.com>
2024-10-17 17:19:55 +0530

    added post and put

--- project/app.go
+++ project/app.go
@@ -86,10 +86,57 @@ func (app *App) getProduct(w http.ResponseWriter, r *http.Request){
 		return 
 	}
 	sendResponse(w, http.StatusOK,p)
+}
+func (app *App) createProduct(w http.ResponseWriter, r *http.Request) {
+	var p product 
 
+	err:= json.NewDecoder(r.Body).Decode(&p)
+
+	if err!=nil{
+		sendError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	err = p.createProduct(app.DB)
+	if err!=nil{
+		sendError(w, http.StatusInternalServerError, "Failed to create product")
+		return 
+	}
+	sendResponse(w, http.StatusOK, p)
+	
+}
+func (app *App) updateProduct(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key, err :=strconv.Atoi(vars["id"])
+
+	if err !=nil{
+		sendError(w, http.StatusBadRequest, "Invalid product ID")
+		return 
+	}
+	var p product
+
+	err = json.NewDecoder(r.Body).Decode(&p)
+	if err!=nil{
+		sendError(w, http.StatusBadRequest, "Invalid request payload")
+		return 
+	}
+	p.ID=key
+
+	err=p.updateProduct(app.DB)
+	if err!=nil{
+		sendError(w,http.StatusInternalServerError, err.Error())
+		return 
+	}
+	sendResponse(w,http.StatusOK,p)
+}
+
+func (app *App) deleteProduct(w http.ResponseWriter, r *http.Request){
+	
 }
 func (app *App) handleRoutes(){
 	app.Router.HandleFunc("/products",app.getProducts).Methods("GET")
 	app.Router.HandleFunc("/product/{id}",app.getProduct).Methods("GET")
+	app.Router.HandleFunc("/product",app.createProduct).Methods("POST")
+	app.Router.HandleFunc("/product/{id}",app.updateProduct).Methods("PUT")
+	app.Router.HandleFunc("/product/{id}",app.deleteProduct).Methods("DELETE")
 
 }
--- project/model.go
+++ project/model.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"errors"
+
 	"log"
 )
 
@@ -54,3 +56,46 @@ func (p *product) getProduct(db *sql.DB) error {
 
 	return nil
 }
+
+//Create product
+// Create product
+func (p *product) createProduct(db *sql.DB) error {
+	// Prepare the insert query using placeholders
+	query := "INSERT INTO products (name, quantity, price) VALUES (?, ?, ?)"
+
+	// Use a prepared statement to execute the query
+	result, err := db.Exec(query, p.Name, p.Quantity, p.Price)
+	if err != nil {
+		return err
+	}
+
+	// Get the last inserted ID
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	// Assign the ID to the product
+	p.ID = int(id)
+	return nil
+}
+
+//update product method
+func (p *product) updateProduct(db *sql.DB) error{
+	query:="UPDATE products SET name=?,quantity=?, price=? WHERE id=?"
+	result,err:=db.Exec(query,p.Name,p.Quantity,p.Price,p.ID)
+	if err!=nil{
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err!=nil{
+		return err
+	}
+	if rowsAffected == 0 {
+		return errors.New("No such row exist")
+	}
+	
+	return nil
+}
+
+
